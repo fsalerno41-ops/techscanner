064be0e275b45621ba83f41a20f2a1a327d24e12
fsalerno41-ops <fsalerno41@gmail.com>
2026-05-23 00:00:57 +0200

    add new products

--- app/data/products.ts
+++ app/data/products.ts
@@ -61,5 +61,71 @@ export const products = [
         url: "https://amazon.it",
       },
     ],
-  },
-]
+  },{{
+  id: 4,
+  slug: "macbook-pro-m4",
+  name: "MacBook Pro M4",
+  image: "/images/macbook.jpg",
+  price: "2199",
+  category: "Laptop",
+  description:
+    "Confronta online il miglior prezzo del MacBook Pro M4.",
+  stores: [
+    {
+      name: "Amazon",
+      price: "2199",
+      url: "https://amazon.it",
+    },
+    {
+      name: "Unieuro",
+      price: "2249",
+      url: "https://unieuro.it",
+    },
+  ],
+},
+
+{
+  id: 5,
+  slug: "playstation-5",
+  name: "PlayStation 5",
+  image: "/images/ps5.jpg",
+  price: "499",
+  category: "Gaming",
+  description:
+    "Trova il miglior prezzo della PlayStation 5.",
+  stores: [
+    {
+      name: "Amazon",
+      price: "499",
+      url: "https://amazon.it",
+    },
+    {
+      name: "Mediaworld",
+      price: "529",
+      url: "https://mediaworld.it",
+    },
+  ],
+},
+
+{
+  id: 6,
+  slug: "samsung-odyssey-g9",
+  name: "Samsung Odyssey G9",
+  image: "/images/g9.jpg",
+  price: "1299",
+  category: "Monitor",
+  description:
+    "Il monitor ultrawide gaming Samsung Odyssey G9.",
+  stores: [
+    {
+      name: "Amazon",
+      price: "1299",
+      url: "https://amazon.it",
+    },
+    {
+      name: "eBay",
+      price: "1349",
+      url: "https://ebay.it",
+    },
+  ],
+},
